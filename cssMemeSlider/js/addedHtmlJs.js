58827f3e32d41d774a3d3ad5d0cb38159fd622ef
Sergei Kudlai <s.kudlai@mail.ru>
2024-09-09 15:55:42 +0300

    feat:added img,JS HTML (09.09.2024 15:55)

--- cssMemeSlider/js/addedHtmlJs.js
+++ cssMemeSlider/js/addedHtmlJs.js
@@ -1,6 +1,29 @@
+import { data } from './data.js';
+
 export function RenderHtml() {
     const pageWrapper = document.querySelector('[data-wrapper]');
+    const htmlSliderInner = document.createElement('div');
 
-    console.log(pageWrapper);
 
+    htmlSliderInner.className = 'slider__inner';
+    htmlSliderInner.setAttribute('data-slider-inner', '');
+    pageWrapper.prepend(htmlSliderInner);
+
+
+    const html = data.map(({ img, title }, index) =>
+        `
+        <article class="slider" data-slider-box data-box-inde=${index}>
+            <h2 class="slider__title">${title}</h2>
+
+            <div class="slider__images-box">
+                <img class="slider__img" src="${img}" alt="">
+            </div>
+
+        </article>    
+         `
+    ).join('');
+
+
+
+    document.querySelector('[data-slider-inner]').insertAdjacentHTML('beforeend', html);
 }
