86aa80a12d04ef88693188d2b19325bdfb3d5499
Sergei Kudlai <s.kudlai@mail.ru>
2024-09-09 16:27:30 +0300

    feat:added dots and ruuber slides (09.09.24 16.27)

--- cssMemeSlider/js/addedHtmlJs.js
+++ cssMemeSlider/js/addedHtmlJs.js
@@ -3,16 +3,23 @@ import { data } from './data.js';
 export function RenderHtml() {
     const pageWrapper = document.querySelector('[data-wrapper]');
     const htmlSliderInner = document.createElement('div');
+    const htmlSlider = document.createElement('div');
 
+
+    htmlSlider.className = 'slider';
+    pageWrapper.prepend(htmlSlider);
+    htmlSlider.setAttribute('data-slider', '');
 
     htmlSliderInner.className = 'slider__inner';
     htmlSliderInner.setAttribute('data-slider-inner', '');
-    pageWrapper.prepend(htmlSliderInner);
+    document.querySelector('[data-slider]').prepend(htmlSliderInner);
+
+
 
 
     const html = data.map(({ img, title }, index) =>
         `
-        <article class="slider" data-slider-box data-box-inde=${index}>
+        <article class="slider__box" data-slider-box data-box-index=${index}>
             <h2 class="slider__title">${title}</h2>
 
             <div class="slider__images-box">
@@ -23,7 +30,18 @@ export function RenderHtml() {
          `
     ).join('');
 
+    const htmlDots = `
+        <div class="slider__dots">
+            ${data.map((_, index) =>
+        `
+            <button class="slider__dots-element" type="button" data-dots-index=${index}>${++index}</button>
+        `
+    ).join('')}
+        </div>
+    `
+
 
 
     document.querySelector('[data-slider-inner]').insertAdjacentHTML('beforeend', html);
+    document.querySelector('[data-slider]').insertAdjacentHTML('beforeend', htmlDots);
 }
